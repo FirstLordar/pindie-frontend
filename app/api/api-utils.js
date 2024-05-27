b7ceb973efaee0fb83da94ec1cfc823ba97debf5
FirstLordar <arzhan2517@gmail.com>
2024-05-27 17:04:17 +0300

    u

--- app/api/api-utils.js
+++ app/api/api-utils.js
@@ -22,7 +22,7 @@ const normalizeDataObject = (obj) => {
   const newObj = JSON.parse(str)
   const result = { ...newObj, category: newObj.categories }
   return result;
-}
+} 
 
 export const normalizeData = (data) => {
   return data.map((item) => {
@@ -30,45 +30,22 @@ export const normalizeData = (data) => {
   })
 }
 
-
 export const getNormalizedGameDataById = async (url, id) => {
   const data = await getData(`${url}/${id}`)
   return isResponseOk(data) ? normalizeDataObject(data) : data
 }
 
-
 export const getNormalizedGamesDataByCategory = async (url, category) => {
-    const data = await getData(`${url}?categories.name=${category}`)
-    return isResponseOk(data) ? normalizeData(data) : data
-
-
-}
-
-
-export const checkIfUserVoted = (game, userId) => {
-  return game.users.find((user) => user.id === userId)
-}
-
-export const vote = async (url, jwt, usersArray) => {
   try {
-    const response = await fetch(url, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${jwt}`,
-      },
-      body: JSON.stringify({ users: usersArray }),
-    });
-    if (response.status !== 200) {
-      throw new Error("Ошибка голосования");
+    const data = await getData(`${url}?categories.name=${category}`)
+    if (!data.length) {
+      throw new Error('Нет игр в категории')
     }
-    const result = await response.json();
-    return result;
+    return isResponseOk(data) ? normalizeData(data) : data
   } catch (error) {
-    return error;
+    return error
   }
-};
-
+}
 
 export const authorize = async (url, data) => {
   try {
@@ -121,3 +98,26 @@ export const getMe = async (url, jwt) => {
   }
 }
 
+export const checkIfUserVoted = (game, userId) => {
+  return game.users.find((user) => user.id === userId)
+}
+
+export const vote = async (url, jwt, usersArray) => {
+  try {
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt}`,
+      },
+      body: JSON.stringify({ users: usersArray })
+    })
+    if (response.status !== 200) {
+      throw new Error('Ошибка голосования')
+    }
+    const result = await response.json()
+    return result
+  } catch (error) {
+    return error
+  }
+}
